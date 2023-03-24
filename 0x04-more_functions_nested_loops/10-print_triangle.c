4640a089aee1bf07a609773cc3a074991e0bb7a6
Okanlawon Olayemi <yemokanlawon@gmail.com>
2023-03-23 18:23:12 -0700

    nested loops, function, prototype, scope of variables, gcc flags and header files cover

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+void print_triangle(int size)
+{
+    if (size <= 0) {
+        _putchar('\n');
+        return;
+    }
+    int i, j;
+    for (i = 1; i <= size; i++) {
+        for (j = 1; j <= i; j++) {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# more function nested loops
+# using _putchar instead of printf, puts
