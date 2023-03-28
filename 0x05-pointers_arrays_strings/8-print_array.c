16341e650b606e503ad97265995f5950ba0f2ca4
Okanlawon Olayemi <yemokanlawon@gmail.com>
2023-03-28 11:02:37 +0100

    prints n elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-print_array
+ * Auth: Okanlawon Olayemi
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array
+ * @a: array name
+ * @n:nuber of elements
+ */
+void print_array(int *a, int n)
+{
+int index;
+
+for (index = 0; index < n; index++)
+{
+printf("%d", a[index]);
+
+if (index == n - 1)
+{
+continue;
+}
+printf(", ");
+}
+printf("\n");
+}
