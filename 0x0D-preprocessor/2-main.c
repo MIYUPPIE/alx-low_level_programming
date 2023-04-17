5a6f892b53ea35aad2c57b8508f93b935538667c
Okanlawon Olayemi <yemokanlawon@gmail.com>
2023-04-17 14:41:59 +0100

    name of the file it was compiled

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+/*
+ * File: 2-main.c
+ * Author: Okanlawon Olayemi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
