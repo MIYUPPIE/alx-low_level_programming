b8ffb088ba1972e54823df6675e4a9c5c479057c
Miyuppie <yemokanlawon@gmail.com>
2023-10-04 19:17:01 +0100

    0x08-recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Okanlawon Olayemi:
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x08-recursion directory.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
