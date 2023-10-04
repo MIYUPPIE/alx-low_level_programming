55f01674e20275b79a49ce8d441eb718ffd9efde
Miyuppie <yemokanlawon@gmail.com>
2023-10-04 19:35:11 +0100

    c-recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * Auth: Okanlawon Olayemi:
+ * Auth: Okanlawon Olayemi
  * Desc: Header file containing declarations for all functions
  *       used in the 0x08-recursion directory.
  */
@@ -11,3 +11,12 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
