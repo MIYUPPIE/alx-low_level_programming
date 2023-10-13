2531cdf1a3135979454995aa9259ca6c045c2a75
Miyuppie <yemokanlawon@gmail.com>
2023-10-13 15:54:43 +0100

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,22 @@
-print_numbers(", ", 4, 0, 98, -1024, 402); // Prints "0, 98, -1024, 402\n"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...) {
+    va_list args;  // Declare a va_list to work with variadic arguments
+
+    va_start(args, n);  // Initialize the va_list
+
+    for (unsigned int i = 0; i < n; i++) {
+        int num = va_arg(args, int);  // Get the next integer argument
+
+        printf("%d", num);  // Print the number
+
+        if (i < n - 1 && separator != NULL) {
+            printf("%s", separator);  // Print the separator if it's not the last number
+        }
+    }
+
+    va_end(args);  // Clean up the va_list
+
+    printf("\n");  // Print a new line at the end
+}
