c5fc9089cc14fe5a306fe42a52c6217ea2bf09af
Miyuppie <yemokanlawon@gmail.com>
2023-10-13 15:55:47 +0100

    prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_all(const char * const format, ...) {
+    va_list args;
+    char *separator = "";
+    unsigned int i = 0;
+
+    va_start(args, format);
+
+    while (format && format[i]) {
+        switch (format[i]) {
+            case 'c':
+                printf("%s%c", separator, va_arg(args, int));
+                break;
+            case 'i':
+                printf("%s%d", separator, va_arg(args, int));
+                break;
+            case 'f':
+                printf("%s%f", separator, (float)va_arg(args, double));
+                break;
+            case 's':
+                {
+                    char *str = va_arg(args, char *);
+                    if (str == NULL) {
+                        printf("%s(nil)", separator);
+                    } else {
+                        printf("%s%s", separator, str);
+                    }
+                }
+                break;
+            default:
+                i++;
+                continue;
+        }
+
+        separator = ", ";
+        i++;
+    }
+
+    va_end(args);
+    printf("\n");
+}
