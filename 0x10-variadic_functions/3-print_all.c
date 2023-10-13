637c314b79cd0baa76de7c8724b70b33fde55f82
Miyuppie <yemokanlawon@gmail.com>
2023-10-13 16:06:45 +0100

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+/* print_all.c */
+#include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 void print_all(const char * const format, ...) {
     va_list args;
@@ -39,5 +41,6 @@ void print_all(const char * const format, ...) {
     }
 
     va_end(args);
+
     printf("\n");
 }
