8851d8d85066638dd820c3ff9e1d535425270e27
Miyuppie <yemokanlawon@gmail.com>
2023-10-13 16:07:12 +0100

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/* Prototype for the sum_them_all function */
 int sum_them_all(const unsigned int n, ...);
+
+/* Prototype for the print_numbers function */
 void print_numbers(const char *separator, const unsigned int n, ...);
+
+/* Prototype for the print_strings function */
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/* Prototype for the print_all function */
 void print_all(const char * const format, ...);
 
-#endif /* MAIN_H */
+#endif /* VARIADIC_FUNCTIONS_H */
