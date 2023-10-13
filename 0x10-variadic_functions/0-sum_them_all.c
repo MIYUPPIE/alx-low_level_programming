882cfafa810fe992cffd2763c661ca483aed3528
Miyuppie <yemokanlawon@gmail.com>
2023-10-13 15:54:22 +0100

    sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,3 +1,20 @@
-int sum;
-sum = sum_them_all(2, 98, 1024); // Returns 1122
-sum = sum_them_all(4, 98, 1024, 402, -1024); // Returns 500
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...) {
+    va_list args;  // Declare a va_list to work with variadic arguments
+    int sum = 0;  // Initialize the sum to 0
+
+    if (n == 0) {
+        return 0;  // If n is 0, return 0
+    }
+
+    va_start(args, n);  // Initialize the va_list
+
+    for (unsigned int i = 0; i < n; i++) {
+        sum += va_arg(args, int);  // Add each argument to the sum
+    }
+
+    va_end(args);  // Clean up the va_list
+
+    return sum;
+}
